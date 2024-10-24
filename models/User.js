2189e713e8fc9d7bfd3e647ceb7a5098dad871a9
juanfel2302 <160626379+juanfel2302@users.noreply.github.com>
2024-10-24 11:58:08 -0700

    Actualización User.js
    Se añadieron los metodos FindByID,
    deleteUserById Y CreateUser

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const connection = require('../db');
 
+
+//Constructor de la clase user
 class User {
   constructor(id_usuario, nombre_usuario, contrasena, es_administrador, estado) {
     this.id_usuario = id_usuario;
@@ -9,6 +11,60 @@ class User {
     this.estado = estado;
   }
 
+  // Método para encontrar un usuario por ID
+  static findById(id_usuario) {
+    return new Promise((resolve, reject) => {
+      const query = 'SELECT * FROM usuarios WHERE id_usuario = ?';
+
+      connection.query(query, [id_usuario], (err, result) => {
+        if (err) {
+          reject(err);
+        } else if (result.length > 0) {
+          const user = new User(
+            result[0].id_usuario,
+            result[0].nombre_usuario,
+            result[0].contrasena,
+            result[0].es_administrador,
+            result[0].estado
+          );
+          resolve(user);
+        } else {
+          resolve(null); // Si no se encuentra el usuario
+        }
+      });
+    });
+  }
+
+  static create(nombre_usuario, contrasena, es_administrador, estado) {
+    return new Promise((resolve, reject) => {
+      const query = 'INSERT INTO usuarios (nombre_usuario, contrasena, es_administrador, estado) VALUES (?, ?, ?, ?)';
+      
+      connection.query(query, [nombre_usuario, contrasena, es_administrador, estado], (err, result) => {
+        if (err) {
+          reject(err); //Si hay un error se rechaza
+        } else {
+          resolve(result.insertId); // Si es exitoso, se regresa el Id del usuario
+        }
+      });
+    });
+  }
+
+  static deleteUserById(id_usuario){
+    return new Promise((resolve, reject) => {
+      const query = 'DELETE FROM usuarios WHERE id_usuario = ?';
+
+      connection.query(query, [id_usuario], (err, result) => {
+        if(err){
+          reject(err);
+        }else if(result.affectedRows > 0){
+          resolve(`Usuario con ID ${id_usuario} eliminado exitosamente.`);
+        }else {
+          resolve(`No se encontró un usuario con ID ${id_usuario}.`);
+        }
+      })
+    })
+  }
 }
+
 
 module.exports = User;
