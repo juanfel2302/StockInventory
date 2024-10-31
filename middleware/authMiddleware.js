3d7ffd81617d92280aa1ae65aacceb36e305af9e
juanfel2302 <160626379+juanfel2302@users.noreply.github.com>
2024-10-30 18:21:31 -0700

    Delete middleware directory

--- middleware/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-function isAuthenticated(req, res, next) {
-    if (req.session && req.session.userId) {
-      return next(); // El usuario está autenticado, continuar a la siguiente función
-    }
-    res.status(401).json({ error: 'Acceso denegado. Debes iniciar sesión.' });
-  }
-  
-  module.exports = isAuthenticated;
-  
